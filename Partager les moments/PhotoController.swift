165255dcfc685114da3b128dddd139781bbe2845
Dea-loC <dealoc@gmail.com>
2018-04-04 15:46:32 +0200

    First

---
+++ Partager les moments/PhotoController.swift
@@ -0,0 +1,26 @@
+//
+//  PhotoController.swift
+//  Partager les moments
+//
+//  Created by Dea-loC on 04/04/2018.
+//  Copyright © 2018 Dea-loC. All rights reserved.
+//
+
+import UIKit
+
+class PhotoController: UIViewController {
+
+    @IBOutlet weak var partagerBouton: UIBarButtonItem!
+    @IBOutlet weak var photoAPartager: UIImageView!
+    @IBOutlet weak var texteAPartager: UITextView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func boutonPartagerAppuyer(_ sender: Any) {
+    }
+    
+}
